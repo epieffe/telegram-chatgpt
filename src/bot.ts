835e1eee0e49ff80b9afb2116ddaed1cff02ff2f
epieffe.eth <ferrariepifanio@gmail.com>
2023-03-12 19:51:11 +0100

    Use regex to filter bot commands and improved logging

--- src/bot.ts
+++ src/bot.ts
@@ -2,27 +2,14 @@ import { Bot } from "grammy";
 
 import * as ai from './ai/openai';
 
-const CHAT_COMMAND = 'chat';
-
 export const bot = new Bot(process.env.TELEGRAM_TOKEN as string);
 
-const onTextMessage = bot.on('message:text');
-
-onTextMessage.command(CHAT_COMMAND, async (ctx) => {
-  const message = getMessage(CHAT_COMMAND, ctx);
-  console.log(`Received chat message=${message}`);
-  if (!message) {
-    console.log("Handling empty message");
-    ctx.reply("Type the /chat command along with your message.\n\nExample: \"/chat what is an NFT?\"");
-    return;
-  }
+bot.hears(/\/chat (.+)/, async (ctx) => {
+  const msg = ctx.match[1];
+  console.log(`Received chat request: user_id=${ctx.from.id} username=${ctx.from.username} chat_id=${ctx.chat.id} chat_type=${ctx.chat.type} message="${msg}"`);
   // Pretend that the bot is typing
   ctx.replyWithChatAction('typing');
   // Retrieve response from AI
-  const response = await ai.chat(message);
+  const response = await ai.chat(msg);
   ctx.reply(response.message);
 });
-
-function getMessage(command: string, ctx): string {
-  return ctx.message.text.slice(command.length + 2).trim();
-}
